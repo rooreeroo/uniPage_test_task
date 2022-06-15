c053fac13e15c8e5a34d654776289a78bc47e233
Vasily S <46477727+rooreeroo@users.noreply.github.com>
2022-06-15 18:24:35 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+ let courses = [
+        { name: "Courses in England", prices: [0, 100] }, 
+        { name: "Courses in Germany", prices: [500, null] }, 
+        { name: "Courses in Italy", prices: [100, 200] }, 
+        { name: "Courses in Russia", prices: [null, 400] },
+        { name: "Courses in China", prices: [50, 250] },
+        { name: "Courses in USA", prices: [200, null] },
+        { name: "Courses in Kazakhstan", prices: [56, 324] },
+        { name: "Courses in France", prices: [null, null] },
+    ];
+
+    // Варианты цен (фильтры), которые ищет пользователь
+    let requiredRange1 = [null, 200];
+    let requiredRange2 = [100, 350];
+    let requiredRange3 = [200, null];
+
+function sortByReq(arr, req) {
+  let result = []
+  if (req[0] === null) {
+   for (let i = 0; i < arr.length; i++) {
+     if ((arr[i].prices[0] !== null && arr[i].prices[0] <= req[1]) || (arr[i].prices[1] !== null && arr[i].prices[1] <= req[1])) {
+       result.push(arr[i])
+     }
+    }
+  } 
+
+  if (req[1] === null) {
+    for (let i = 0; i < arr.length; i++) {
+     if ((arr[i].prices[1] !== null && arr[i].prices[1] >= req[0]) || (arr[i].prices[0] !== null && arr[i].prices[0] >= req[0])) {
+       result.push(arr[i])
+     }
+    }
+  }
+  if ((req[0] !== null && req[1] !== null)) {
+    for (let i = 0; i < arr.length; i++) {
+     if ((arr[i].prices[1] >= req[0] && arr[i].prices[0] <= req[0]) && (arr[i].prices[0] <= req[1] && arr[i].prices[1] <= req[1])) {
+       result.push(arr[i])
+     }
+    }
+  } 
+  return result
+}
+console.log(sortByReq(courses, requiredRange3))
